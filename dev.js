e4a4901dec6e3345a192b36499db49c67d791e1d
Gary Anderson <ganderson@seattleacademy.org>
2020-03-24 05:24:45 -0700

    Update files

---
+++ dev.js
@@ -0,0 +1,28 @@
+
+//Developer code for creating input boxes
+$("img").mousedown(onmousedown);
+var iLeft = 0;
+var iTop = 0;
+var counter = 0;
+
+function onmousedown(evt) {
+  let str = `<input style='position:absolute;left:${iLeft}px;top:${iTop}px;width:${evt.pageX -
+    iLeft}px;height:${evt.pageY - iTop}px' data-correct=''/>`;
+  iLeft = evt.pageX;
+  iTop = evt.pageY;
+  counter ++;
+  if(counter%2==0)
+  $('#area').val($('#area').val()+str+'\n');
+}
+
+var lastTime = 0;
+var aud = document.getElementById("myAudio");
+
+aud.onpause = function() {
+	  let str = `data-start='${lastTime.toFixed(2)}' data-duration='${((aud.currentTime - lastTime)*1000).toFixed(0)}'`;
+      $('#area').val($('#area').val()+str+'\n');
+};
+
+aud.onplay = function() {
+	lastTime = aud.currentTime;
+};
--- script.js
+++ script.js
@@ -3,41 +3,40 @@ console.clear();
 $("input").change(onChange);
 
 function onChange(evt) {
-  let correct = $(this).data("correct");
-  let response = $(this).val();
-  if (correct == response) {
-    $(this)
-      .removeClass("incorrect")
-      .addClass("correct");
-  } else {
-    $(this)
-      .removeClass("correct")
-      .addClass("incorrect");
-  }
+    let correct = $(this).data("correct");
+    let response = $(this).val();
+    if (correct == response) {
+        $(this)
+            .removeClass("incorrect")
+            .addClass("correct");
+    } else {
+        $(this)
+            .removeClass("correct")
+            .addClass("incorrect");
+    }
 }
 
 //Play sound segment when enter is pressed
 $("input").keypress(function(event) {
-  if (event.key == "Enter") {
-    let start = $(this).data("start");
-    if (!start) return;
-    var x = document.getElementById("myAudio");
-    x.currentTime = start;
-    x.play();
-    let duration = $(this).data("duration");
-    if (!duration) return;
-    setTimeout(() => x.pause(), duration);
-  }
-});
+    if (event.key == "Enter") {
+        console.log(event)
+        if (event.shiftKey) {
+          console.log('shift')
+            $("input").each(function() {
 
-//Developer code for creating input boxes
-$("img").mousedown(onmousedown);
-var iLeft = 0,
-  iTop = 0;
-function onmousedown(evt) {
-  let str = `<input style='position:absolute;left:${iLeft}px;top:${iTop}px;width:${evt.pageX -
-    iLeft}px;height:${evt.pageY - iTop}px' data-correct=''/>`;
-  iLeft = evt.pageX;
-  iTop = evt.pageY;
-  console.table(str);
-}
+                correct = $(this).data('correct');
+                console.log(correct)
+
+                $(this).val(correct);
+            })
+        }
+        let start = $(this).data("start");
+        if (!start) return;
+        var x = document.getElementById("myAudio");
+        x.currentTime = start;
+        x.play();
+        let duration = $(this).data("duration");
+        if (!duration) return;
+        setTimeout(() => x.pause(), duration);
+    }
+});
