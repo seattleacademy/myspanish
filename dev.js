5c97c3770f0a975e6d12d891657784467786bb24
Gary Anderson <ganderson@seattleacademy.org>
2020-04-14 09:57:16 -0700

    Add page 57 and update script for SpanishDict.com

--- dev.js
+++ dev.js
@@ -1,27 +1,77 @@
 //Developer code for creating input boxes
 $("img").mousedown(onmousedown);
+$("img").mouseup(onmouseup);
 var iLeft = 0;
+var pLeft = 1;
+var pTop = 1;
 var iTop = 0;
 var counter = 0;
 
+// function onmousedown(evt) {
+//   let pWidth = (evt.offsetX/evt.target.width)-pLeft;
+//   let pHeight = (evt.offsetY/evt.target.height)-pTop;
+//   let l = `${(pLeft*100).toFixed(2)}%`   
+//   let t = `${(pTop*100).toFixed(2)}%`   
+//   let w = `${(pWidth*100).toFixed(2)}%`   
+//   let h = `${(pHeight*100).toFixed(2)}%`   
+//   let str = `<input style='position:absolute;left:${l};top:${t};width:${w};height:${h}' data-correct=''/>`;
+//   pLeft = evt.offsetX/evt.target.width
+//   pTop = evt.offsetY/evt.target.height
+//   counter ++;
+//   if(counter%2==0)
+//   $('#area').val($('#area').val()+str+'\n');
+// return false;
+// }
+
 function onmousedown(evt) {
-  let str = `<input style='position:absolute;left:${iLeft}px;top:${iTop}px;width:${evt.pageX -
-    iLeft}px;height:${evt.pageY - iTop}px' data-correct=''/>`;
-  iLeft = evt.pageX;
-  iTop = evt.pageY;
-  counter ++;
-  if(counter%2==0)
-  $('#area').val($('#area').val()+str+'\n');
+    pLeft = evt.offsetX / evt.target.width
+    pTop = evt.offsetY / evt.target.height
+    return false;
+}
+$("body").on('change', '#makeme', makeinput);
+
+function makeinput(evt) {
+    //console.log($(this).val())
+    //$("#makeme").data("greeting", "Hello World");
+    //$('#makeme').data('correct',$(this).val());
+    let theData = `data-correct="${$(this).val()}"`
+    if (lastTime) theData += ` data-start='${lastTime.toFixed(2)}'`;
+
+    if (lastDuration) theData += ` data-duration='${((aud.currentTime - lastTime)*1000).toFixed(0)}'`
+    let str = $('#makeme').prop('outerHTML');
+    str = str.replace('id="makeme"', theData);
+    //console.log(str)
+    $('#area').val($('#area').val() + str + '\n');
+    $("#myAudio").focus();
+    //return false;
 }
 
+function onmouseup(evt) {
+    let pWidth = (evt.offsetX / evt.target.width) - pLeft;
+    let pHeight = (evt.offsetY / evt.target.height) - pTop;
+    let l = `${(pLeft*100).toFixed(2)}%`
+    let t = `${(pTop*100).toFixed(2)}%`
+    let w = `${(pWidth*100).toFixed(2)}%`
+    let h = `${(pHeight*100).toFixed(2)}%`
+    $('#makeme').css('left', l);
+    $('#makeme').css('top', t);
+    $('#makeme').css('width', w);
+    $('#makeme').css('height', h);
+    $("#makeme").focus();
+    $('#makeme').val('');
+
+
+}
 var lastTime = 0;
+var lastDuration = 0;
 var aud = document.getElementById("myAudio");
 
 aud.onpause = function() {
-	  let str = `data-start='${lastTime.toFixed(2)}' data-duration='${((aud.currentTime - lastTime)*1000).toFixed(0)}'`;
-      $('#area').val($('#area').val()+str+'\n');
+    lastDuration = ((aud.currentTime - lastTime) * 1000).toFixed(0);
+    //let str = `data-start='${lastTime.toFixed(2)}' data-duration='${((aud.currentTime - lastTime)*1000).toFixed(0)}'`;
+    // $('#area').val($('#area').val()+str+'\n');
 };
 
 aud.onplay = function() {
-	lastTime = aud.currentTime;	      
+    lastTime = aud.currentTime;
 };
--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 console.clear();
+        let word = {
+            spword : "palabra",
+            left : "25%" ,
+            top: "10%" ,
+            image: "57.png"
+
+        }
+        console.log(word);
 
 $("input").change(onChange);
 
@@ -6,6 +14,12 @@ $("input").change(onChange);
 
 function onChange(evt) {
     let correct = $(this).data("correct");
+    if (!correct) {
+        $(this)
+            .removeClass("incorrect")
+            .removeClass("correct");
+        return;
+    }
     let response = $(this).val();
     if (correct.toLowerCase() == response.toLowerCase()) {
         $(this)
@@ -29,20 +43,31 @@ $("input").keypress(function(event) {
         if (event.shiftKey) {
             $("input").each(function() {
                 correct = $(this).data('correct');
-                console.log(correct)
+                // console.log(correct)
                 $(this).val(correct);
             })
+            return;
         }
+
+        if (event.ctrlKey) {
+            const correct = $(this).data('correct');
+            let url = `https://www.spanishdict.com/conjugate/${correct}`;
+            // console.log(url)
+            window.open(url, '_blank');
+            return;
+        }
+        var myObj = { name: "John", age: 31, city: "New York" };
         let start = $(this).data("start");
         if (!start) return;
+        let num = $("#audiospeed").val();
+        if (num) aud.playbackRate = num
+        else num = 1;
         var x = document.getElementById("myAudio");
         x.currentTime = start;
         x.play();
         let duration = $(this).data("duration");
         if (!duration) return;
-        let num = $("#audiospeed").val();
-        if (num) aud.playbackRate = num
-        else num = 1;
-        setTimeout(() => x.pause(), duration/num);
+
+        setTimeout(() => x.pause(), duration / num);
     }
 });
